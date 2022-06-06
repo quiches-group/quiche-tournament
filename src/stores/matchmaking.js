fe8d3aff0f4c7cc2f2b16cdcb9cf815bcff5c826
Cannonbark <cannonbark@pop-os.localdomain>
2022-06-06 18:08:48 +0200

    Managing odd number of players and winners

--- src/stores/matchmaking.js
+++ src/stores/matchmaking.js
@@ -1,72 +1,81 @@
 import { defineStore } from "pinia";
-import { ref } from "vue";
+import { reactive } from "vue";
 
 export const useMatchmaking = defineStore("matchmaking", () => {
-  const players = [
-    {
-      id: 1,
-      name: "Player 1",
-    },
-    {
-      id: 2,
-      name: "Player 2",
-    },
-    {
-      id: 3,
-      name: "Player 3",
-    },
-    {
-      id: 4,
-      name: "Player 4",
-    },
-    {
-      id: 5,
-      name: "Player 5",
-    },
-    {
-      id: 6,
-      name: "Player 6",
-    },
-    {
-      id: 7,
-      name: "Player 7",
-    },
-    {
-      id: 8,
-      name: "Player 8",
-    },
-  ];
+  const state = reactive({
+    players: [
+      {
+        id: 1,
+        name: "Player 1",
+      },
+      {
+        id: 2,
+        name: "Player 2",
+      },
+      {
+        id: 3,
+        name: "Player 3",
+      },
+      {
+        id: 4,
+        name: "Player 4",
+      },
+      {
+        id: 5,
+        name: "Player 5",
+      },
+      {
+        id: 6,
+        name: "Player 6",
+      },
+      {
+        id: 7,
+        name: "Player 7",
+      },
+      {
+        id: 8,
+        name: "Player 8",
+      },
+    ],
+    playersRemaining: [],
+    battles: [],
+  });
 
-  const remainingPlayers = [...players];
-
-  const battles = ref([]);
-
-  function get() {
-    return battles.value;
+  function setWinner(player) {
+    state.playersRemaining.push(player);
   }
 
-  function createBattle(...playersList) {
-    battles.value.push({
-      id: battles.value.length + 1,
+  function createBattle(round, ...playersList) {
+    state.battles[round].push({
+      id: state.battles.length + 1,
       players: playersList,
     });
   }
 
   function randomChoice() {
-    const choice = Math.floor(Math.random() * remainingPlayers.length);
-    const player = remainingPlayers[choice];
-    remainingPlayers.splice(choice, 1);
+    const choice = Math.floor(Math.random() * state.playersRemaining.length);
+    const player = state.playersRemaining[choice];
+    state.playersRemaining.splice(choice, 1);
     return player;
   }
 
-  function create() {
-    if (remainingPlayers.length % 2 === 0) {
-      while (remainingPlayers.length > 0) {
-        console.log(remainingPlayers.length);
-        createBattle(randomChoice(), randomChoice());
-      }
+  function create(playersList = undefined) {
+    if (playersList) {
+      state.players = playersList;
     }
+    state.playersRemaining = state.players;
+
+    const round = state.battles.length;
+    state.battles[round] = [];
+
+    while (state.playersRemaining.length > 1) {
+      createBattle(round, randomChoice(), randomChoice());
+    }
+
+    console.log(state.playersRemaining);
   }
 
-  return { get, create };
+  const { battles, players } = state;
+
+  return { create, setWinner, battles, players };
 });

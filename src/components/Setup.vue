72022422dbbacd9bfda6323e246be4f67e92c00b
Nicolas Barbosa <nicolas.barbosa@orange.com>
2022-06-13 16:33:54 +0200

    add players list logic, add and remove dynamically

--- src/components/Setup.vue
+++ src/components/Setup.vue
@@ -2,23 +2,45 @@
   <!--
   <q-input placeholder="nom" v-model="player1"></q-input>
 -->
-  <div class="flex justify-center h-screen">
+  <div class="flex justify-center">
     <q-dropdown
       placeholder="Nombre de joueur"
       :options="possibleNumberPlayer"
-      @select="selectItem"
+      @select="selectNumberOfPlayer"
     ></q-dropdown>
+  </div>
+  <div class="flex justify-center">
+    <ul>
+      <li class="w-20" v-for="player in playerList" :key="player.id">
+        <label>Joueur {{ player.id }}</label>
+        <q-input placeholder="Prénom" v-model="player.name"></q-input>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script setup>
-import { reactive } from "vue";
+import { ref } from "vue";
 
 const possibleNumberPlayer = ["2", "3", "4", "5", "6", "7", "8"];
-const selectedNumberPlayer = reactive({ number: 2 });
-const selectItem = (item) => {
-  /* eslint-disable no-console */
-  selectedNumberPlayer.number = item;
+
+const playerList = ref([]);
+
+const selectNumberOfPlayer = (numberOfPlayer) => {
+  for (let i = 1; i <= numberOfPlayer; i += 1) {
+    if (playerList.value[i - 1] === undefined) {
+      const newPlayer = {
+        id: i,
+        name: "",
+      };
+      playerList.value.push(newPlayer);
+    } else if (numberOfPlayer < playerList.value.length) {
+      const playersToRemove = playerList.value.length - numberOfPlayer;
+      for (let i = 0; i < playersToRemove; i += 1) {
+        playerList.value.pop();
+      }
+    }
+  }
 };
 </script>
 

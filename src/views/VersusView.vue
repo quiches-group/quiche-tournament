3541fcb41c76d7d2de4d7c74706eec95ec9a443e
Nicolas Barbosa <nicolas.barbosa@orange.com>
2022-06-20 09:55:27 +0200

    Remove and recreate versusView

--- src/views/VersusView.vue
+++ src/views/VersusView.vue
@@ -30,44 +30,35 @@ import { ref, reactive, computed } from "vue";
 import { useRoute } from "vue-router";
 import { useTournaments } from "@/stores/tournaments.js";
 import RoundVersus from "../components/RoundVersus.vue";
-
 const route = useRoute();
 const tournament = useTournaments();
-
 const actualTournament = ref(tournament.get(route.params.tournamentId));
-
 const actualRound = computed(() => {
   return actualTournament.value.actualRound().value;
 });
-
 const actualBattle = computed(() => {
   return actualRound.value.actualBattle().value;
 });
-
 const players = computed(() => {
   return actualBattle.value.players;
 });
-
 const isTournamentEnd = computed(() => {
   return (
-    actualTournament.value.podium.length ===
+      actualTournament.value.podium.length ===
       actualTournament.value.players.length ||
-    actualTournament.value.podium.length === 4
+      actualTournament.value.podium.length === 4
   );
 });
-
 const winner = reactive({
   id: undefined,
   name: undefined,
   victory: undefined,
 });
-
 const setWinner = (newWinner) => {
   winner.id = newWinner.id;
   winner.name = newWinner.name;
   winner.victory = newWinner.victory;
 };
-
 function nextBattle() {
   actualRound.value.win(actualBattle.value.id, winner.id);
   setWinner({
@@ -76,7 +67,6 @@ function nextBattle() {
     victory: "",
   });
 }
-
 function endTournament() {
   actualRound.value.win(actualBattle.value.id, winner.id);
 }
@@ -86,22 +76,18 @@ function endTournament() {
 .match {
   @apply h-screen flex flex-col justify-evenly py-32;
 }
-
 .tournamentInfo {
   @apply flex flex-col justify-center content-center;
 }
-
 .tournamentStep {
   @apply text-center text-white text-3xl font-semibold
   underline underline-offset-2 decoration-quiche-purple/50 decoration-4
   md:text-5xl;
 }
-
 .roundStep {
   @apply text-center text-white text-2xl
   py-5;
 }
-
 .centered {
   @apply flex justify-center;
 }

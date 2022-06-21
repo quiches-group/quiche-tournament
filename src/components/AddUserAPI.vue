3aaa47d185f1d509ae583bf6447dc040e2ef3819
hugo <hugo.bordais@ynov.com>
2022-06-21 15:01:58 +0200

    Exercice supp - Hugo

---
+++ src/components/AddUserAPI.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="container">
+    <input placeholder="Prenom"/>
+    <input placeholder="Nom"/>
+    <input placeholder="Framework"/>
+    <q-button @click="getUsers()">See each user on the console</q-button>
+    <q-button @click="postUsers()">Valider</q-button>
+  </div>
+</template>
+
+<script setup>
+import axios from "axios";
+
+const getUsers =() => {
+  const getData = axios.get("https://4078-92-103-197-34.eu.ngrok.io/users");
+  console.log(getData)
+}
+
+const postUsers =() => {
+  axios.post(`https://4078-92-103-197-34.eu.ngrok.io/users`, {
+    body: {
+      firstname: "Prénom",
+      lastname: "Nom",
+      framework: "Framework",
+    },
+  })
+}
+</script>
